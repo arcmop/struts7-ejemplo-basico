7d8bd5af73ed19397167f26668092959e1382802
mastermind <mastermind@local.localhost>
2026-01-31 20:26:45 -0500

    feat: Formularios

--- src/main/java/local/unp/desarrolloweb2/tienda/action/IndexAction.java
+++ src/main/java/local/unp/desarrolloweb2/tienda/action/IndexAction.java
@@ -1,10 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package local.unp.desarrolloweb2.tienda.action;
 
 import org.apache.struts2.ActionSupport;
+import org.apache.struts2.interceptor.parameter.StrutsParameter;
 
 /**
  *
@@ -12,9 +9,44 @@ import org.apache.struts2.ActionSupport;
  */
 public class IndexAction extends ActionSupport {
 
+    private String numero1;
+    private String numero2;
+    private Integer resultado;
+
     @Override
     public String execute() throws Exception {
         return SUCCESS;
+    }
+
+    public String sumar() {
+        resultado = Integer.parseInt(numero1) + Integer.parseInt(numero2);
+        return SUCCESS;
+    }
+
+    public String getNumero1() {
+        return numero1;
+    }
+
+    @StrutsParameter
+    public void setNumero1(String numero1) {
+        this.numero1 = numero1;
+    }
+
+    public String getNumero2() {
+        return numero2;
+    }
+
+    @StrutsParameter
+    public void setNumero2(String numero2) {
+        this.numero2 = numero2;
+    }
+
+    public Integer getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(Integer resultado) {
+        this.resultado = resultado;
     }
 
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -2,11 +2,28 @@
 <%@ taglib prefix="s" uri="/struts-tags" %>
 
 <html>
-    <body>
-        <h2>Hello World Struts 2!</h2>
-        <p>
-            <s:a action="saludar" > Saludar a Usuario</s:a>
-        </p>
-    </body>
-</html>
 
+    <body>
+        <h2>Calculadora Suma</h2>
+
+        <s:if test="hasActionErrors()">
+            <div style="color:red">
+                <s:actionerror />
+            </div>
+        </s:if>
+
+        <s:form action="sumar">
+            <s:textfield name="numero1" label="Número 1" />
+            <s:textfield name="numero2" label="Número 2" />
+            <s:submit value="Sumar" />
+        </s:form>
+
+        <s:if test="resultado != null">
+            <h3>Resultado:
+                <s:property value="resultado" />
+            </h3>
+        </s:if>
+
+    </body>
+
+</html>
